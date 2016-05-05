a40228833552840978d05247a78a7f3a9d24fb50
natelohn <nlohn@stanford.edu>
2016-05-05 05:09:52 -0700

    all groupsVC table populated with users groups

--- AllGroupsVC.swift
+++ AllGroupsVC.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import RealmSwift
 
 class AllGroupsVC: UIViewController {
     
+    @IBOutlet weak var groupTableView: UITableView!
+    
+    let db = DataBase()
     var username = ""
+    var user = User()
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         print("user: \(username)")
+        user = db.getUserFromDB(username)
+        groupTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        user.printAllGroups()
     }
+    
+    
+    
+    //table view logic
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return user.groups.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = groupTableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        cell.textLabel!.text = user.groups[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("Row \(indexPath.row) selected")
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50 //height of the post table cell in the xib file
+    }
+    
+    
     
 
 }
